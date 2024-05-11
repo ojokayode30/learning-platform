4e2ffa2278c45df2ed0940f79ec6cc6b2adbb216
Kayode Ojo <ojokayode30@gmail.com>
2024-05-11 13:12:44 +0100

    latest

--- flaskr/resource.py
+++ flaskr/resource.py
@@ -4,7 +4,6 @@ from flask import (Blueprint, flash, g, redirect, render_template, request, sess
 from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 from flaskr.auth import login_required
-from meta_ai_api import MetaAI
 
 bp = Blueprint('resource', __name__, url_prefix='/resource')
 
