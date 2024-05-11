85c395747bf3300007cc76d3ae5089e2b889cf54
Kayode Ojo <ojokayode30@gmail.com>
2024-05-11 13:35:20 +0100

    meta

--- flaskr/ai_chat.py
+++ flaskr/ai_chat.py
@@ -5,7 +5,7 @@ from flask import (Blueprint, flash, g, redirect, render_template, request, sess
 from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 from flaskr.auth import login_required
-from .lib.meta_ai_api import MetaAI
+from .lib.meta_ai_api.main import MetaAI
 
 bp = Blueprint('chat', __name__, url_prefix='/chat')
 
